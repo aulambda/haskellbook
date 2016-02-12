2f46f4792ea9c4f3865d5b67b743d3bf6eae6f04
Gerald Aulandez <gerald.aulandez@gmail.com>
2016-02-12 16:01:29 -0600

    added final exercise from chapter 08

---
+++ 08-recursion/wordnumber.hs
@@ -0,0 +1,17 @@
+-- wordnumber.hs
+module WordNumber where
+
+import Data.List (intersperse)
+
+digitToWord :: Int -> String
+digitToWord n = numNames !! n
+  where
+    numNames = ["zero","one","two","three","four", "five", "six", "seven", "eight", "nine"]
+
+digits :: Int -> [Int]
+digits x
+  |x < 10 = [x]
+  |otherwise = digits (div x 10) ++ [mod x 10]
+
+wordNumber :: Int -> String
+wordNumber n = concat . intersperse "-" . map digitToWord $ digits n
